572960a6acb3b27e6e2d68e3d1dc34fe4a97678d
Ravishka Dulshan <92871474+Ravishka2000@users.noreply.github.com>
2023-05-16 14:20:58 +0530

    aa

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(cors({
     origin: [
         "http://localhost:3000",
-        "https://thambapanni-app.onrender.com"
+        "https://thambapanni.onrender.com"
     ]
 }));
 
